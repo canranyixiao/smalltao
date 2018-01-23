701f8fff0e6a67c4f7b9468677bee80904fd4472
canranyixiao <pengtaonian@163.com>
2018-01-23 22:10:46 +0800

    第一天

---
+++ SSH_01/src/com/itheima/service/impl/UserServiceImpl.java
@@ -0,0 +1,12 @@
+package com.itheima.service.impl;
+
+import com.itheima.service.UserService;
+
+public class UserServiceImpl implements UserService {
+
+	@Override
+	public void save() {
+		System.out.println("调用UserServiceImpl的save方法~！~@！");
+	}
+
+}
---
+++ SSH_01/src/com/itheima/web/action/UserAction.java
@@ -0,0 +1,23 @@
+package com.itheima.web.action;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.itheima.service.UserService;
+import com.opensymphony.xwork2.ActionSupport;
+
+public class UserAction extends ActionSupport {
+	
+	public String save(){
+		System.out.println("调用了UserAction的save方法~~~");
+		
+		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		UserService userService = (UserService) context.getBean("userService");
+		
+		userService.save();
+		
+		return NONE;
+	}
+
+}
